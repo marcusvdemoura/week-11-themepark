a08c803a077e41b5b988bbcbebd813b684a1f132
marcusvdemoura <marcusdemoura@outlook.com>
2020-12-01 12:52:19 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int peopleInLine = 90;
+        int peopleOnRide = 45;
+        int time = 0;
+
+        while (peopleInLine < 600){
+            peopleInLine -= peopleOnRide;
+            peopleInLine+=63;
+            time+=5;
+        }
+        System.out.println(time);
+
+  }
+}
